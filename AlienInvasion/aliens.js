39f32af5343c6251809960a3dda98f26a70f8eca
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2022-04-15 15:14:29 -0400

    Added arrays of months and weekdays. Added querySelectors.

---
+++ AlienInvasion/aliens.js
@@ -0,0 +1,32 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  //Get the first element with class="alien1".
+  const alien1 = document.querySelector(".alien1");
+  //Get the first element with class="deadline".
+  const deadline = document.querySelector(".deadline");
+  //Get the first element with class="alien1".
+  //and grabbing the header 4 within that div.
+  const items = document.querySelectorAll(".deadline-format h4");
+  console.log(items);
