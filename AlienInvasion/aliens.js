9df7d129cdddc93d23c20e6cdd19e337b7b433b4
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2022-04-23 00:39:18 -0400

    Added the methods getFullYear(), getHours(), getMinutes(), etc. to the countdown.

--- AlienInvasion/aliens.js
+++ AlienInvasion/aliens.js
@@ -26,7 +26,29 @@ const months = [
   const alien1 = document.querySelector(".alien1");
   //Get the first element with class="deadline".
   const deadline = document.querySelector(".deadline");
-  //Get the first element with class="alien1".
-  //and grabbing the header 4 within that div.
+  /*Get the first element with class="alien1".
+  and grabbing the header 4 within that div.
   const items = document.querySelectorAll(".deadline-format h4");
-  console.log(items);
+  */
+  /*
+  new Date(); Will add the current date.
+  If I want a specific date then I need to add these values inside
+  the date();
+  NOTICE! If I want to select the month of May, I need to write 4
+  instead of 5 (being the month number). Because months are 0 based.
+  */
+  let futureDate = new Date(2022, 6, 24, 17, 30, 0);
+  console.log(futureDate);
+
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+
+  let month = futureDate.getMonth();
+  month = months[month]
+  console.log(months[month]);
+
+  alien1.textContent = 
+  `giveway ends on ${month} ${year} ${hours}:${minutes}am`;
+
+  //Stopped video at 5:19
