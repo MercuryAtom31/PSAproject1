7685847bc2b458079baaec90f8f7f8a84bb66ed3
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2022-05-03 21:29:29 -0400

    Added nav bar

--- AlienInvasion/aliens.js
+++ AlienInvasion/aliens.js
@@ -63,13 +63,12 @@ let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 /**
- * 
+ *
  * To get the current date only, use the date() method
  * without arguments.
  */
 // months are ZERO index based;
 const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
-
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -81,32 +80,30 @@ const date = futureDate.getDate();
 
 const weekday = weekdays[futureDate.getDay()];
 
-
 //console.log(months[month]);
 
 alien1.textContent = `Possibility to be rescued ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
 // Future time in ms.
 const futureTime = futureDate.getTime();
 
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+  //1s = 1000ms
+  //1m = 60s
+  //1hr = 60minutes
+  //1d = 24hrs
 
-function getRemainingTime(){
-const today = new Date().getTime();
-const t = futureTime - today;
-console.log(t);
-//1s = 1000ms
-//1m = 60s
-//1hr = 60minutes
-//1d = 24hrs
-
-//Values in ms (how many milliseconds are in one day).
-const oneDay = 24 * 60 * 60 * 1000;
-//How many milliseconds are in an hour.
-const oneHour = 60 * 60 * 1000;
-//How many milliseconds are in a minute.
-const oneMinute = 60 * 1000;
-//Calculating all values.
-let days = t / oneDay;
-/*
+  //Values in ms (how many milliseconds are in one day).
+  const oneDay = 24 * 60 * 60 * 1000;
+  //How many milliseconds are in an hour.
+  const oneHour = 60 * 60 * 1000;
+  //How many milliseconds are in a minute.
+  const oneMinute = 60 * 1000;
+  //Calculating all values.
+  let days = t / oneDay;
+  /*
   The Math.floor() function returns 
   the largest integer less than or equal to a given number.
   e.g.
@@ -122,30 +119,29 @@ let days = t / oneDay;
  console.log(Math.floor(-5.05));
  expected output: -6
 */
-days = Math.floor(days);
-let hours = Math.floor((t % oneDay) / oneHour);
-let minutes = Math.floor((t % oneHour) / oneMinute);
-let seconds = Math.floor((t % oneMinute) / 1000);
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
 
-//set values arrays.
-const values = [days, hours, minutes, seconds];
+  //set values arrays.
+  const values = [days, hours, minutes, seconds];
 
-function format (item){
-  if (item < 10){
-    return (item = `0${item}`);
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
   }
-  return item;
-}
 
-items.forEach(function(item, index){
-  item.innerHTML = values[index];
-});
+  items.forEach(function (item, index) {
+    item.innerHTML = values[index];
+  });
 
-if (t < 0) {
-  clearInterval(countdown);
-  deadline.innerHTML = `<h4 class="expired">sorry, aliens have arrived!</h4>`;
-}
-
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, aliens have arrived!</h4>`;
+  }
 }
 
 //countdown.
@@ -153,7 +149,6 @@ let countdown = setInterval(getRemainingTime, 1000);
 //Set initial values.
 getRemainingTime();
 
-
 //5:40:30
 
-//giveaway = alien1. 
+//giveaway = alien1.
