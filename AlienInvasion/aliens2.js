6975d697f46c9caf008997144eb6fb93f3ac8b80
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2022-05-06 23:07:09 -0400

    Display content and hiding content.

--- AlienInvasion/aliens2.js
+++ AlienInvasion/aliens2.js
@@ -1,3 +1,6 @@
+/**
+ * Form script.
+ */
 let userAnswers = new Array(5);
 
 const correctAnswer = ["c", "b", "b", "b", "a"];
@@ -16,3 +19,42 @@ function score() {
 
   alert("Here is your survival rate: " + correctCount + " out of 100.");
 }
+
+/**
+ * Text appear and disappear script.
+ */
+
+ function display(idName) {
+  let team = document.getElementById(idName);
+  team.style.display = "block";
+}
+
+function hide(idName) {
+  let team = document.getElementById(idName);
+  team.style.display = "none";
+}
+
+//Adding the event listeners of each function.
+let el1 = document.getElementById("padres");
+el1.addEventListener("mouseover", function () {
+  display("padresID");
+});
+el1.addEventListener("mouseout", function () {
+  hide("padresID");
+});
+
+let el2 = document.getElementById("yankees");
+el2.addEventListener("mouseover", function () {
+  display("yankeesID");
+});
+el2.addEventListener("mouseout", function () {
+  hide("yankeesID");
+});
+
+let el3 = document.getElementById("redsox");
+el3.addEventListener("mouseover", function () {
+  display("redsoxID");
+});
+el3.addEventListener("mouseout", function () {
+  hide("redsoxID");
+});
