956444172ac34a367ee40b11253b77ca9341e042
williammarcotte <billmarcotte125@gmail.com>
2022-05-07 12:33:53 -0400

    Added night/day mode button

--- SatientCars/cars.js
+++ SatientCars/cars.js
@@ -1,12 +1,38 @@
 var a = setInterval(casNum,getRandTime());
-let num = 10324;
 let cas = document.getElementById("casualties");
+let num = 10324
+let mode = 0;
+/*if(localStorage.getItem('deaths') != null){
+    let num = localStorage.getItem('casualties');
+    cas.textContent = num;
+    window.alert(num);
+}
+else if(localStorage.getItem('deaths') == null){
+    let num = 10324
+}*/
+
 
 function casNum(){
     num += Math.floor(Math.random()*20);
     cas.textContent = num;
+    //localStorage.setItem('casualties',num);
 }
 
 function getRandTime(){
-    return Math.floor(Math.random() * (10000) )
+    return Math.floor(Math.random() * (7000) )
+}
+
+function change(){
+    if(mode==0){
+        let col = document.getElementById('wrapper');
+        col.style.color='aliceblue';
+        col.style.backgroundColor='black';
+        mode=1;
+    }
+    else if(mode==1){
+        let col = document.getElementById('wrapper');
+        col.style.color='black';
+        col.style.backgroundColor='aliceblue';
+        mode=0;
+    }
 }
